cf3b5fb1b008193e0ad124bcbf22484083b62f45
wiph2004 <149805523+wiph2004@users.noreply.github.com>
2023-12-08 14:51:36 -0600

    adding updated code

--- assets/script.js
+++ assets/script.js
@@ -12,25 +12,135 @@ function generatePassword(){
   var userChoiceNum = confirm("Would you like numbers?");
   var userChoiceSpecial = confirm("would you like special characters?");
 
-  console.log(userChoicelower);
-  console.log(userChoiceUpper);
-  console.log(userChoiceNum);
-  console.log(userChoiceSpecial);
   var passwordInitial = [];
+  var mainPasswordList = [];
 
-if (userChoicelower && userChoiceUpper && userChoiceNum && userChoiceSpecial){
-  for(x = 0; x < userChoiceLength; x++){
-    var MainPasswordList = lowercase.concat(upperCase, Nums, Specials);
-    var computerRandomIndex = Math.floor(Math.random() * (MainPasswordList.length - 1));
-    console.log(computerRandomIndex)
-    passwordInitial.push(MainPasswordList[computerRandomIndex]);
-      console.log(passwordInitial);
-    
+  if (userChoicelower) {
+    mainPasswordList = mainPasswordList.concat(lowercase);
   }
 
+  if (userChoiceUpper) {
+    mainPasswordList = mainPasswordList.concat(upperCase);
+  }
+
+  if (userChoiceNum) {
+    mainPasswordList = mainPasswordList.concat(Nums);
+  }
+
+  if (userChoiceSpecial) {
+    mainPasswordList = mainPasswordList.concat(Specials);
+  }
+
+  if (mainPasswordList.length === 0) {
+    alert("CHOOSE SOMETHING!");
+    return;
+  } 
+
+  console.log("MAIN PASSWORD LIST", mainPasswordList);
+  function getPassword() {
+    var password = [];
+    for(x = 0; x < userChoiceLength; x++) {
+      var computerRandomIndex = Math.floor(Math.random() * (mainPasswordList.length - 1));
+      console.log(computerRandomIndex);
+      password.push(mainPasswordList[computerRandomIndex]);
+      console.log(password);
+    }
+
+    return password;
+  }
+
+  var isAccurate = false;
+  while(!isAccurate) {
+    passwordInitial = getPassword();
+
+    if (userChoicelower && includesLower(passwordInitial)) {
+      isAccurate = true;
+    } else {
+      isAccurate = false;
+      continue;
+    }
+
+    if (userChoiceUpper && includesUpper(passwordInitial)) {
+      isAccurate = true;
+    } else {
+      isAccurate = false;
+      continue;
+    }
+
+    if (userChoiceNum && includesNumbers(passwordInitial)) {
+      isAccurate = true;
+    } else {
+      isAccurate = false;
+      continue;
+    }
+
+    if (userChoiceSpecial && includesSpecials(passwordInitial)) {
+      isAccurate = true;
+    } else {
+      isAccurate = false;
+      continue;
+    }
+  }
+
+  return passwordInitial.join('');
 }
 
+function includesLower(password) {
+  for(var y=0; y < lowercase.length; y++){
+    if (password.includes(lowercase[y])) {
+      return true;
+    }
+  }
+  return false;
 }
+
+function includesUpper(password) {
+  for(var y=0; y < upperCase.length; y++){
+    password.includes(upperCase[y])
+    return true;
+  }
+  return false;
+}
+
+function includesNumbers(password) {
+  for(var y=0; y < Nums.length; y++){
+    password.includes(Nums[y])
+    return true;
+  }
+  return false;
+}
+
+function includesSpecials(password) {
+  for(var y=0; y < Specials.length; y++){
+    password.includes(Specials[y])
+    return true;
+  }
+  return false;
+}
+
+// if (userChoicelower && userChoiceUpper && userChoiceNum && userChoiceSpecial){
+//   for(x = 0; x < userChoiceLength; x++){
+//     var mainPasswordList = lowercase.concat(upperCase, Nums, Specials);
+//     var computerRandomIndex = Math.floor(Math.random() * (mainPasswordList.length - 1));
+//     console.log(computerRandomIndex)
+//     passwordInitial.push(mainPasswordList[computerRandomIndex]);
+      
+//     for(y=0; y < lowercase.length; y++){
+//       passwordInitial.includes(lowercase[y])
+//     }
+//     for(y=0; y < upperCase.length; y++){
+//       passwordInitial.includes(upperCase[y])
+//     }
+//     for(y=0; y < Nums.length; y++){
+//       passwordInitial.includes(Nums[y])
+//     }
+//     for(y=0; y < Specials.length; y++){
+//       passwordInitial.includes(Specials[y])
+//     }
+//   }
+
+// }
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
